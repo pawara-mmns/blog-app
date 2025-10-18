ecddc78e6da1422150cfd6859b8aea93b7d49bc2
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-10-18 08:53:20 +0530

    initial commit

---
+++ src/test/java/com/social/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
